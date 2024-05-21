e34b5881a2b8692e4fafd89716eacaee4eb84cf4
Aggelos Papanagiotou <aggepap@gmail.com>
2024-05-21 20:40:23 +0300

    tested and changed

--- src/gr/aueb/cf/Hello2App.java
+++ src/gr/aueb/cf/Hello2App.java
@@ -2,6 +2,6 @@ package gr.aueb.cf;
 
 public class Hello2App {
     public static void main(String[] args) {
-        System.out.println("Hello Again!");
+        System.out.println("Hello Again and again!");
     }
 }
